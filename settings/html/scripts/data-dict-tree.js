88a02f5f6149eb77d03568fdb0ff6f77cf3dab91
rocex <rocexwang@gmail.com>
2020-04-29 10:10:28 +0800

    Update favicon.ico, index.html, and 14 more files...

---
+++ settings/html/scripts/data-dict-tree.js
@@ -0,0 +1,71 @@
+var dataDictIndexData = [
+	{
+		id: -1,
+		name: '介绍',
+		open: true,
+		url: './intro.html',
+		target: 'DataDictFrame'
+	},
+	{
+		id: 0,
+		name: '0',
+		open: true
+	},
+	{
+		id: 1,
+		pId: 0,
+		name: '1',
+		url: './new.html',
+		target: 'DataDictFrame'
+	},
+	{
+		id: 2,
+		open: true,
+		name: '2',
+		url: './right.html',
+		target: 'DataDictFrame'
+	},
+	{
+		id: 3,
+		open: true,
+		name: '3',
+		url: './new.html',
+		target: 'DataDictFrame'
+	},
+	{
+		id: 4,
+		pId: 3,
+		name: '4',
+		url: './new.html',
+		target: 'DataDictFrame'
+	},
+	{
+		id: 5,
+		open: true,
+		name: '5',
+		url: './new.html',
+		target: 'DataDictFrame'
+	},
+	{
+		id: 6,
+		pId: 2,
+		name: '6',
+		url: './new.html',
+		target: 'DataDictFrame'
+	},
+	{
+		id: 7,
+		pId: 1,
+		open: true,
+		name: '7',
+		url: './new.html',
+		target: 'DataDictFrame'
+	},
+	{
+		id: 8,
+		pId: 3,
+		name: '8',
+		url: './new.html',
+		target: 'DataDictFrame'
+	}
+];
