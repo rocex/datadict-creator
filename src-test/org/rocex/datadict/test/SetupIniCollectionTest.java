b15ec07a1d5304c5c6057d18079c1ce9653b24cf
rocex <rocexwang@gmail.com>
2025-05-28 08:57:50 +0800

    Update SetupIniCollectionTest.java

--- src-test/org/rocex/datadict/test/SetupIniCollectionTest.java
+++ src-test/org/rocex/datadict/test/SetupIniCollectionTest.java
@@ -18,36 +18,41 @@ import java.util.Properties;
 public class SetupIniCollectionTest
 {
     Path pathFrom = Paths.get("C:/dist/");
-
+    
     public void testCollectSetupIni() throws IOException
     {
+        if (!pathFrom.toFile().exists())
+        {
+            return;
+        }
+
         Files.walkFileTree(pathFrom, new SimpleFileVisitor<>()
         {
             Path pathParent = pathFrom;
-
+            
             public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes attrs) throws IOException
             {
                 if (pathParent.compareTo(path.getParent()) != 0)
                 {
                     pathParent = path.getParent();
                 }
-
+                
                 Path pathFile = Paths.get(path.toString(), "setup.ini");
-
+                
                 if (pathFile.toFile().exists())
                 {
                     Properties prop = new Properties();
-                    
-                    prop.load(Files.newBufferedReader(pathFile, Charset.forName("CP936")));//
 
-                    String strFold = pathFile.getParent().getFileName().toString();
+                    prop.load(Files.newBufferedReader(pathFile, Charset.forName("CP936")));//
+                    
+                    String strDir = pathFile.getParent().getFileName().toString();
                     String strCode = prop.getProperty("code");
                     String strName = prop.getProperty("name");
-
-                    System.out.printf("%-8s %-20s %-20s %s \n", strCode, strFold, pathFrom.compareTo(pathParent) == 0 ? "root" : pathParent.toFile().getName(),
+                    
+                    System.out.printf("%-8s %-20s %-20s %s \n", strCode, strDir, pathFrom.compareTo(pathParent) == 0 ? "root" : pathParent.toFile().getName(),
                             strName);
                 }
-
+                
                 return FileVisitResult.CONTINUE;
             }
         });
